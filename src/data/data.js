3a8598490cd675f61aa3bd9f625c6267201a37fe
Ahmet Hüseyin Dok <ahmet.dok@qbitra.com>
2022-02-22 14:19:51 +0300

    created dummy data

---
+++ src/data/data.js
@@ -0,0 +1,68 @@
+export const data = {
+    categories: [
+        {
+            name: 'Kategori 1',
+            image: 'https://www.nizampide.com/wp-content/uploads/2018/07/kar%C4%B1%C5%9F%C4%B1k-kebap-%C4%B1zgara-porsiyon-nizam-pide-s%C3%BCtla%C3%A7-istanbul-beyo%C4%9Flu-istiklal-caddesi-600x600.jpg',
+            items: [
+                {
+                    name: 'Item 1',
+                    price: '15 tl',
+                    image: 'https://productimages.hepsiburada.net/s/34/375/10450126176306.jpg'
+                },
+                {
+                    name: 'Item 2',
+                    price: '30 tl',
+                    image: 'https://cdn.getiryemek.com/restaurants/1608798504016_1125x522.jpeg'
+                },
+                {
+                    name: 'Item 3',
+                    price: '13 tl',
+                    image: 'https://cdn.yemek.com/mncrop/620/388/uploads/2020/04/lahmacun-hazirlik-10.jpg'
+                }
+            ]
+        },
+        {
+            name: 'Kategori 2',
+            image: 'https://cdn.yemek.com/mncrop/940/625/uploads/2016/02/osmanli-serbetleri-4.jpg',
+            items: [
+                {
+                    name: 'Item 1',
+                    price: '15 tl',
+                    image: 'https://productimages.hepsiburada.net/s/34/375/10450126176306.jpg'
+                },
+                {
+                    name: 'Item 2',
+                    price: '30 tl',
+                    image: 'https://cdn.getiryemek.com/restaurants/1608798504016_1125x522.jpeg'
+                },
+                {
+                    name: 'Item 3',
+                    price: '13 tl',
+                    image: 'https://cdn.yemek.com/mncrop/620/388/uploads/2020/04/lahmacun-hazirlik-10.jpg'
+                }
+            ]
+        },
+        {
+            name: 'Kategori 3',
+            image: 'https://i20.haber7.net/resize/1240x720//haber/haber7/photos/2020/14/mevsiminde_beslenmenin_onemi_ve_nisan_ayinda_tuketilmesi_gereken_sebze_ve_meyveler_nelerdir_1585810736_2142.jpg',
+            items: [
+                {
+                    name: 'Item 1',
+                    price: '15 tl',
+                    image: 'https://productimages.hepsiburada.net/s/34/375/10450126176306.jpg'
+                },
+                {
+                    name: 'Item 2',
+                    price: '30 tl',
+                    image: 'https://cdn.getiryemek.com/restaurants/1608798504016_1125x522.jpeg'
+                },
+                {
+                    name: 'Item 3',
+                    price: '13 tl',
+                    image: 'https://cdn.yemek.com/mncrop/620/388/uploads/2020/04/lahmacun-hazirlik-10.jpg'
+                }
+            ]
+        }
+    ]
+}
+
