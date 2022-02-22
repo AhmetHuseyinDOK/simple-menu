221c6e1728ce5de2589345dc059690e372a33422
Ahmet Hüseyin Dok <ahmet.dok@qbitra.com>
2022-02-22 15:47:06 +0300

    updated dummy data

--- src/data/data.js
+++ src/data/data.js
@@ -26,17 +26,17 @@ export default   {
             image: 'https://cdn.yemek.com/mncrop/940/625/uploads/2016/02/osmanli-serbetleri-4.jpg',
             items: [
                 {
-                    name: 'Item 1',
+                    name: 'Item 4',
                     price: '15 tl',
                     image: 'https://productimages.hepsiburada.net/s/34/375/10450126176306.jpg'
                 },
                 {
-                    name: 'Item 2',
+                    name: 'Item 5',
                     price: '30 tl',
                     image: 'https://cdn.getiryemek.com/restaurants/1608798504016_1125x522.jpeg'
                 },
                 {
-                    name: 'Item 3',
+                    name: 'Item 6',
                     price: '13 tl',
                     image: 'https://cdn.yemek.com/mncrop/620/388/uploads/2020/04/lahmacun-hazirlik-10.jpg'
                 }
@@ -47,17 +47,17 @@ export default   {
             image: 'https://i20.haber7.net/resize/1240x720//haber/haber7/photos/2020/14/mevsiminde_beslenmenin_onemi_ve_nisan_ayinda_tuketilmesi_gereken_sebze_ve_meyveler_nelerdir_1585810736_2142.jpg',
             items: [
                 {
-                    name: 'Item 1',
+                    name: 'Item 7',
                     price: '15 tl',
                     image: 'https://productimages.hepsiburada.net/s/34/375/10450126176306.jpg'
                 },
                 {
-                    name: 'Item 2',
+                    name: 'Item 8',
                     price: '30 tl',
                     image: 'https://cdn.getiryemek.com/restaurants/1608798504016_1125x522.jpeg'
                 },
                 {
-                    name: 'Item 3',
+                    name: 'Item 9',
                     price: '13 tl',
                     image: 'https://cdn.yemek.com/mncrop/620/388/uploads/2020/04/lahmacun-hazirlik-10.jpg'
                 }
